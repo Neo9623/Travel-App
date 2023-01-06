aae7f5dacf1b7156edf95f69c3bb31f3561864da
Neo Tsehlana <54308778+Neo9623@users.noreply.github.com>
2023-01-06 13:14:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import "./components/Navbar";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
